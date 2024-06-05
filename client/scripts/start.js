ab178e66da73855c14c7357c071701128bf7425d
RAR <sswwqqaa@gmail.com>
2024-06-05 18:30:39 +0200

    fix: Fixed dev server stop

--- client/scripts/start.js
+++ client/scripts/start.js
@@ -109,7 +109,7 @@ checkBrowsers(paths.appPath, isInteractive)
 
     ['SIGINT', 'SIGTERM'].forEach(function (sig) {
       process.on(sig, function () {
-        devServer.close();
+        devServer.stop();
         process.exit();
       });
     });
@@ -117,7 +117,7 @@ checkBrowsers(paths.appPath, isInteractive)
     if (process.env.CI !== 'true') {
       // Gracefully exit when stdin ends
       process.stdin.on('end', function () {
-        devServer.close();
+        devServer.stop();
         process.exit();
       });
     }
