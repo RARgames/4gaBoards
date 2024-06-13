f98f79185203f3b5bba44a150f22cd43c2a30376
RAR <sswwqqaa@gmail.com>
2024-06-13 19:58:34 +0200

    clean: Removed old useform, useField hooks

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,6 +1,4 @@
-import useField from './use-field';
 import useField2 from './use-field2';
-import useForm from './use-form';
 import useForm2 from './use-form2';
 import useSteps from './use-steps';
 import useLocalStorage from './use-local-storage';
@@ -8,4 +6,4 @@ import useModal from './use-modal';
 import useClosableForm from './use-closable-form';
 import useResizeObserverSize from './use-resize-observer-size';
 
-export { useField, useField2, useForm, useForm2, useSteps, useLocalStorage, useModal, useClosableForm, useResizeObserverSize };
+export { useField2, useForm2, useSteps, useLocalStorage, useModal, useClosableForm, useResizeObserverSize };
