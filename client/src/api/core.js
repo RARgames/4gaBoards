5c2f15164cb11e91347a6ac06431f6109d50c2cc
RAR <sswwqqaa@gmail.com>
2025-02-20 00:06:16 +0100

    fix: updateCoreSettings using sockets

--- client/src/api/core.js
+++ client/src/api/core.js
@@ -1,10 +1,11 @@
 import http from './http';
+import socket from './socket';
 
 /* Actions */
 
 const getCoreSettingsPublic = (headers) => http.get('/core-settings-public', undefined, headers);
 
-const updateCoreSettings = (data, headers) => http.post('/core-settings', data, headers);
+const updateCoreSettings = (data, headers) => socket.post('/core-settings', data, headers);
 
 export default {
   getCoreSettingsPublic,
