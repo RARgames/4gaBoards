3a2ffabf1c7c7fda3a71fbe02fe9f7a2dbaa1928
RAR <sswwqqaa@gmail.com>
2025-04-23 16:28:05 +0200

    fix: MDEditor handle links without switching card/opening description

--- client/src/components/Utils/Markdown/MDPreview.jsx
+++ client/src/components/Utils/Markdown/MDPreview.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
@@ -9,10 +9,20 @@ import MDSettings from './MDSettings';
 const MDPreview = React.forwardRef(({ source, isGithubConnected, githubRepo, className, ...props }, ref) => {
   const remarkPlugins = isGithubConnected ? [[remarkGithub, { repository: githubRepo }]] : null;
 
+  const handleClick = useCallback((e) => {
+    const link = e.target?.closest('a');
+    if (link) {
+      e.stopPropagation(); // Prevent opening MDEditor, prevent card switch in ListView
+    }
+  }, []);
+
   return (
     // TODO temp removed s.preview
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <MDEditor.Markdown ref={ref} source={source} remarkPlugins={remarkPlugins} rehypePlugins={MDSettings.rehypePlugins} className={classNames(className)} {...props} />
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div onClick={handleClick}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <MDEditor.Markdown ref={ref} source={source} remarkPlugins={remarkPlugins} rehypePlugins={MDSettings.rehypePlugins} className={classNames(className)} {...props} />
+    </div>
   );
 });
 
