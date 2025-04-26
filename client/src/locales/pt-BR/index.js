41145f9f378615f6583e3f679531534c771e85d8
RAR <sswwqqaa@gmail.com>
2025-04-26 22:17:22 +0200

    fix: Fixed pt-BR translation always loaded by default

--- client/src/locales/pt-BR/index.js
+++ client/src/locales/pt-BR/index.js
@@ -1,12 +1,9 @@
-import merge from 'lodash/merge';
-
-import core from './core';
 import login from './login';
 
 export default {
   language: 'pt-BR',
   country: 'br',
   name: 'Português',
-  embeddedLocale: merge(login, core),
+  embeddedLocale: login,
   flags: ['BR', 'PT'],
 };
