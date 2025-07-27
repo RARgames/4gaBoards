ddda6b585a04c3ddbb6a29a96bfef652c2dc0591
Esquivar <s26294@pjwstk.edu.pl>
2025-07-27 21:27:54 +0200

    Add Mail controller (create, show, update), model, routes, and migration

---
+++ server/db/migrations/20250724142831_create_mails_table.js
@@ -0,0 +1,25 @@
+module.exports.up = (knex) =>
+  knex.schema.createTable('mail', (table) => {
+    /* Columns */
+
+    table.bigInteger('id').primary().defaultTo(knex.raw('next_id()'));
+
+    table.text('mail_id').notNullable().unique();
+
+    table.bigInteger('user_id').notNullable();
+    table.bigInteger('project_id').notNullable();
+    table.bigInteger('board_id').notNullable();
+    table.bigInteger('list_id').notNullable();
+
+    table.timestamp('created_at', true);
+    table.timestamp('updated_at', true);
+
+    /* Indexes */
+
+    table.index('user_id');
+    table.index('project_id');
+    table.index('board_id');
+    table.index('list_id');
+  });
+
+module.exports.down = (knex) => knex.schema.dropTable('mail');
