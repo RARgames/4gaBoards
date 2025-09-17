00276872b713e0b09d8d56c3deda4533a5349176
RAR <sswwqqaa@gmail.com>
2025-09-17 13:13:53 +0200

    feat: Added PWA negative fallback, disabled scroll to reload PWA

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -594,6 +594,7 @@ module.exports = function (webpackEnv) {
           // to make lazy-loading failure scenarios less likely.
           // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
           maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+          navigateFallback: '/',
         }),
       // TypeScript type checking
       useTypeScript &&
