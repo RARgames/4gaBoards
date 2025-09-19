758212937c27b96eedf8506307def9d23b22f7c1
RAR <sswwqqaa@gmail.com>
2025-09-19 17:56:35 +0200

    fix: Register service worker only in production

--- client/src/index.js
+++ client/src/index.js
@@ -9,15 +9,8 @@ import './i18n';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(React.createElement(Root, { store, history }));
 
-if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined' && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registration successful:', registration); // eslint-disable-line no-console
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error); // eslint-disable-line no-console
-      });
+    navigator.serviceWorker.register('/service-worker.js').catch(() => {});
   });
 }
