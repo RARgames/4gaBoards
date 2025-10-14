cd50dde05aa110e766a0f5d710bbd279aa8d905d
RAR <sswwqqaa@gmail.com>
2025-10-14 20:54:28 +0200

    fix: Removed some deprecated eslint rules

--- eslint-configs/base.js
+++ eslint-configs/base.js
@@ -387,10 +387,6 @@ module.exports = [
       // https://eslint.org/docs/rules/no-return-assign
       'no-return-assign': ['error', 'always'],
 
-      // disallow redundant `return await`
-      // https://eslint.org/docs/rules/no-return-await
-      'no-return-await': 'error',
-
       // disallow use of `javascript:` urls.
       // https://eslint.org/docs/rules/no-script-url
       'no-script-url': 'error',
@@ -877,22 +873,13 @@ module.exports = [
       //endregion
 
       //region node
-      // enforce return after a callback
-      'callback-return': 'off',
-
       // require all requires be top-level
       // https://eslint.org/docs/rules/global-require
       'global-require': 'error',
 
-      // enforces error handling in callbacks (node environment)
-      'handle-callback-err': 'off',
-
       // disallow use of the Buffer() constructor
       // https://eslint.org/docs/rules/no-buffer-constructor
       'no-buffer-constructor': 'error',
-
-      // disallow mixing regular variable and require declarations
-      'no-mixed-requires': ['off', false],
 
       // disallow use of new operator with the require function
       'no-new-require': 'error',
@@ -900,18 +887,6 @@ module.exports = [
       // disallow string concatenation with __dirname and __filename
       // https://eslint.org/docs/rules/no-path-concat
       'no-path-concat': 'error',
-
-      // disallow use of process.env
-      'no-process-env': 'off',
-
-      // disallow process.exit()
-      'no-process-exit': 'off',
-
-      // restrict usage of specified node modules
-      'no-restricted-modules': 'off',
-
-      // disallow use of synchronous methods (off by default)
-      'no-sync': 'off',
       //endregion
 
       //region strict
@@ -1285,7 +1260,7 @@ module.exports = [
       'no-nested-ternary': 'error',
 
       // disallow use of the Object constructor
-      'no-new-object': 'error',
+      'no-object-constructor': 'error',
 
       // disallow use of unary operators, ++ and --
       // https://eslint.org/docs/rules/no-plusplus
@@ -1503,9 +1478,6 @@ module.exports = [
       //region variables
       // enforce or disallow variable initializations at definition
       'init-declarations': 'off',
-
-      // disallow the catch clause parameter name being the same as a variable in the outer scope
-      'no-catch-shadow': 'off',
 
       // disallow labels that share a name with a variable
       // https://eslint.org/docs/rules/no-label-var
--- eslint-configs/client.js
+++ eslint-configs/client.js
@@ -129,9 +129,6 @@ module.exports = [
         },
       ],
 
-      // Deprecated in favor of react/jsx-sort-props
-      'react/jsx-sort-prop-types': 'off',
-
       // Enforce props alphabetical sorting
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
       'react/jsx-sort-props': [
@@ -387,11 +384,6 @@ module.exports = [
           beforeClosing: 'never',
         },
       ],
-
-      // Enforce spaces before the closing bracket of self-closing JSX elements
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-space-before-closing.md
-      // Deprecated in favor of jsx-tag-spacing
-      'react/jsx-space-before-closing': ['off', 'always'],
 
       // Prevent usage of Array index in keys
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-array-index-key.md
