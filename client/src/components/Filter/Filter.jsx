3426b8e927fd9ce77b9fecbf39d73c24e489fffd
RAR <sswwqqaa@gmail.com>
2025-11-14 18:24:07 +0100

    reafactor: Updated Filter to match filterNotification

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useRef, useEffect } from 'react';
+import React, { useCallback, useRef, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
 import Paths from '../../constants/Paths';
 import { useField } from '../../hooks';
-import { useToggle } from '../../lib/hooks';
 import { Button, ButtonStyle, Input, Form, Icon, IconType, IconSize } from '../Utils';
 
 import * as s from './Filter.module.scss';
@@ -13,18 +12,17 @@ const Filter = React.memo(({ defaultValue, projects, filteredProjects, path, onC
   const [t] = useTranslation();
   const field = useRef(null);
   const [value, handleFieldChange, setValue, handleFocus] = useField(defaultValue);
-  const [isTargetBoard, toggleFilterTarget] = useToggle();
+  const targets = useMemo(() => ['project', 'board'], []);
+  const [target, setTarget] = useState('project');
+
   const prevPathRef = useRef();
 
   const submit = useCallback(
-    (val, overrideTarget = false) => {
-      if ((!overrideTarget && isTargetBoard) || (overrideTarget && !isTargetBoard)) {
-        onChangeFilterQuery({ query: val, target: 'board' });
-      } else {
-        onChangeFilterQuery({ query: val, target: 'project' });
-      }
+    (val, overrideTarget) => {
+      const selected = overrideTarget || target;
+      onChangeFilterQuery({ query: val, target: selected });
     },
-    [isTargetBoard, onChangeFilterQuery],
+    [target, onChangeFilterQuery],
   );
 
   const handleSubmit = useCallback(() => {
@@ -51,10 +49,12 @@ const Filter = React.memo(({ defaultValue, projects, filteredProjects, path, onC
   );
 
   const handleToggleClick = useCallback(() => {
-    toggleFilterTarget();
-    submit(value, true);
+    const idx = targets.indexOf(target);
+    const next = targets[(idx + 1) % targets.length];
+    setTarget(next);
+    submit(value, next);
     field.current?.focus();
-  }, [submit, toggleFilterTarget, value]);
+  }, [targets, target, submit, value]);
 
   const handleKeyDown = useCallback(
     (e) => {
@@ -78,25 +78,25 @@ const Filter = React.memo(({ defaultValue, projects, filteredProjects, path, onC
   useEffect(() => {
     if (prevPathRef.current !== path) {
       if (path === Paths.ROOT) {
-        if (isTargetBoard) {
-          toggleFilterTarget();
+        if (target === 'board') {
+          setTarget('project');
         }
         handleCancel();
         field.current?.focus();
       } else if (path === Paths.PROJECTS) {
-        if (!isTargetBoard) {
-          toggleFilterTarget();
+        if (target !== 'board') {
+          setTarget('board');
         }
         handleCancel();
         field.current?.focus();
       }
     }
     prevPathRef.current = path;
-  }, [handleCancel, isTargetBoard, onFilterQueryClear, path, toggleFilterTarget]);
+  }, [handleCancel, target, onFilterQueryClear, path, setTarget]);
 
   const getCounterText = () => {
     if (value !== '') {
-      if (isTargetBoard) {
+      if (target === 'board') {
         const boardsCount = filteredProjects.reduce((sum, project) => sum + (project.boards ? project.boards.length : 0), 0);
         return `${t('common.boards', { count: boardsCount })} (${filteredProjects.length}/${t('common.projects', { count: projects.length })})`;
       }
@@ -105,12 +105,14 @@ const Filter = React.memo(({ defaultValue, projects, filteredProjects, path, onC
     return '';
   };
 
+  const upperCasedTarget = target.charAt(0).toUpperCase() + target.slice(1);
+
   return (
     <div>
       <Form className={s.form} onKeyDown={handleKeyDown}>
-        <Input ref={field} value={value} className={s.field} onChange={handleChange} placeholder={isTargetBoard ? t('common.filterBoards') : t('common.filterProjects')} onFocus={handleFocus} />
+        <Input ref={field} value={value} className={s.field} onChange={handleChange} placeholder={t(`common.filter${upperCasedTarget}s`)} onFocus={handleFocus} />
         <Button style={ButtonStyle.Icon} title={t('common.toggleFilter')} onClick={handleToggleClick} className={s.inputButton} tabIndex="-1">
-          <div className={s.inputButtonText}>{isTargetBoard ? 'B' : 'P'}</div> <Icon type={IconType.Switch} size={IconSize.Size13} />
+          <div className={s.inputButtonText}>{t(`common.filterId${upperCasedTarget}`)[0].toUpperCase()}</div> <Icon type={IconType.Switch} size={IconSize.Size13} />
         </Button>
       </Form>
       <div className={s.controls}>
