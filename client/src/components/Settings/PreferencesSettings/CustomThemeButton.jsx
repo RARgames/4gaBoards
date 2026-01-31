c75566066dd361067015f88bbbd9dd062914076b
RAR <sswwqqaa@gmail.com>
2026-01-31 20:41:48 +0100

    cleanup: Started colors cleanup (for custom themes)

--- client/src/components/Settings/PreferencesSettings/CustomThemeButton.jsx
+++ client/src/components/Settings/PreferencesSettings/CustomThemeButton.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router';
-import clsx from 'clsx';
-import { t } from 'i18next';
 
 import Paths from '../../../constants/Paths';
 import { Button, ButtonStyle } from '../../Utils';
@@ -9,6 +8,8 @@ import { Button, ButtonStyle } from '../../Utils';
 import * as s from './CustomThemeButton.module.scss';
 
 const CustomThemeButton = React.memo(() => {
+  const [t] = useTranslation();
+
   return (
     <Link to={Paths.SETTINGS_PREFERENCES_THEME}>
       <Button style={ButtonStyle.DefaultBorder} className={s.themeButton} content={t('common.goToThemeEditor')} />
