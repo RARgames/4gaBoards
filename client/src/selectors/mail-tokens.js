7e9fc6fcbc24b70cac05728371e9bbb4bcd688ec
RAR <sswwqqaa@gmail.com>
2026-02-24 18:48:58 +0100

    feat: Improved mail tokens selectors

--- client/src/selectors/mail-tokens.js
+++ client/src/selectors/mail-tokens.js
@@ -1,133 +1,102 @@
 import { createSelector } from 'redux-orm';
 
 import orm from '../orm';
-import getMeta from '../utils/get-meta';
-import { selectCurrentUserId } from './users';
-
-export const makeSelectMailTokenForCurrentUserByListId = () =>
-  createSelector(
-    orm,
-    (state) => selectCurrentUserId(state),
-    (_, listId) => listId,
-    (session, currentUserId, listId) => {
-      if (!currentUserId || !listId) return null;
-      const { MailToken } = session;
-      if (!MailToken) return null;
-      const mailTokenModel = MailToken.all()
-        .toModelArray()
-        .find((mailToken) => mailToken.listId === listId && mailToken.userId === currentUserId);
-      if (!mailTokenModel) return null;
-      return { ...mailTokenModel.ref, ...getMeta(mailTokenModel) };
-    },
-  );
-export const selectMailTokenForCurrentUserByListId = makeSelectMailTokenForCurrentUserByListId();
 
 export const makeSelectMailTokensByListId = () =>
   createSelector(
     orm,
     (_, listId) => listId,
-    (session, listId) => {
-      if (!listId) return [];
+    ({ List }, listId) => {
+      if (!listId) {
+        return listId;
+      }
 
-      const { MailToken } = session;
-      if (!MailToken) return [];
+      const listModel = List.withId(listId);
+      if (!listModel) {
+        return listModel;
+      }
 
-      const mailTokenModels = MailToken.all().filter({ listId }).toModelArray();
-
-      return mailTokenModels.map((mailTokenModel) => ({
+      const mailTokens = listModel.getOrderedMailTokensModelArray().map((mailTokenModel) => ({
         ...mailTokenModel.ref,
-        ...getMeta(mailTokenModel),
         user: mailTokenModel.user?.ref || null,
       }));
+      return mailTokens;
     },
   );
 
 export const selectMailTokensByListId = makeSelectMailTokensByListId();
 
-export const makeSelectMailTokenForCurrentUserByBoardId = () =>
+export const makeSelectMailTokenCountByListId = () =>
   createSelector(
     orm,
-    (state) => selectCurrentUserId(state),
-    (_, boardId) => boardId,
-    (session, currentUserId, boardId) => {
-      if (!currentUserId || !boardId) return null;
-      const { MailToken } = session;
-      if (!MailToken) return null;
+    (_, listId) => listId,
+    ({ List }, listId) => {
+      if (!listId) {
+        return listId;
+      }
 
-      const mailTokenModel = MailToken.all()
-        .toModelArray()
-        .find((mailToken) => mailToken.boardId === boardId && mailToken.userId === currentUserId && (mailToken.listId === null || mailToken.listId === undefined));
+      const listModel = List.withId(listId);
+      if (!listModel) {
+        return listModel;
+      }
 
-      if (!mailTokenModel) return null;
-
-      return { ...mailTokenModel.ref, ...getMeta(mailTokenModel) };
+      return listModel.getOrderedMailTokensQuerySet().count();
     },
   );
 
-export const selectMailTokenForCurrentUserByBoardId = makeSelectMailTokenForCurrentUserByBoardId();
-
-export const makeSelectMailTokenCountForBoardId = () =>
-  createSelector(
-    orm,
-    (_, boardId) => boardId,
-    (session, boardId) => {
-      if (!boardId) return 0;
-
-      const { MailToken } = session;
-      if (!MailToken) return 0;
-
-      const mailTokenModels = MailToken.all()
-        .filter({ boardId })
-        .toModelArray()
-        .filter((mailToken) => mailToken.listId === null || mailToken.listId === undefined);
-
-      return mailTokenModels.length;
-    },
-  );
-
-export const selectMailTokenCountForBoardId = makeSelectMailTokenCountForBoardId();
+export const selectMailTokenCountByListId = makeSelectMailTokenCountByListId();
 
 export const makeSelectMailTokensByBoardId = () =>
   createSelector(
     orm,
     (_, boardId) => boardId,
-    (session, boardId) => {
-      if (!boardId) return [];
-
-      const { MailToken, List, Board } = session;
-      if (!MailToken) return [];
+    ({ Board }, boardId) => {
+      if (!boardId) {
+        return boardId;
+      }
 
       const boardModel = Board.withId(boardId);
+      if (!boardModel) {
+        return boardModel;
+      }
 
-      const mailTokenModels = MailToken.all().filter({ boardId }).toModelArray();
-
-      return mailTokenModels.map((mailTokenModel) => {
-        const isBoardLevel = mailTokenModel.listId === null || mailTokenModel.listId === undefined;
-
-        const listModel = isBoardLevel ? null : List.withId(mailTokenModel.listId);
-
-        return {
-          ...mailTokenModel.ref,
-          ...getMeta(mailTokenModel),
-          user: mailTokenModel.user?.ref || null,
-          contextType: isBoardLevel ? 'board' : 'list',
-          contextName: isBoardLevel ? boardModel?.name || 'Board' : listModel?.name || 'List',
-        };
-      });
+      const mailTokens = boardModel.getOrderedMailTokensModelArray().map((mailTokenModel) => ({
+        ...mailTokenModel.ref,
+        user: mailTokenModel.user?.ref || null,
+      }));
+      return mailTokens;
     },
   );
 
 export const selectMailTokensByBoardId = makeSelectMailTokensByBoardId();
 
+export const makeSelectMailTokenCountByBoardId = () =>
+  createSelector(
+    orm,
+    (_, boardId) => boardId,
+    ({ Board }, boardId) => {
+      if (!boardId) {
+        return boardId;
+      }
+
+      const boardModel = Board.withId(boardId);
+      if (!boardModel) {
+        return boardModel;
+      }
+
+      return boardModel.getOrderedMailTokensQuerySet().count();
+    },
+  );
+
+export const selectMailTokenCountByBoardId = makeSelectMailTokenCountByBoardId();
+
 export default {
-  makeSelectMailTokenForCurrentUserByListId,
-  selectMailTokenForCurrentUserByListId,
   makeSelectMailTokensByListId,
   selectMailTokensByListId,
-  makeSelectMailTokenForCurrentUserByBoardId,
-  selectMailTokenForCurrentUserByBoardId,
+  makeSelectMailTokenCountByListId,
+  selectMailTokenCountByListId,
   makeSelectMailTokensByBoardId,
   selectMailTokensByBoardId,
-  makeSelectMailTokenCountForBoardId,
-  selectMailTokenCountForBoardId,
+  makeSelectMailTokenCountByBoardId,
+  selectMailTokenCountByBoardId,
 };
