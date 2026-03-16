a6202fefe3e46ddd8693ff1a762d7b5550b060ea
RAR <sswwqqaa@gmail.com>
2026-03-16 23:01:28 +0100

    docs: Removed top comment from routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,13 +1,3 @@
-/**
- * Route Mappings
- * (sails.config.routes)
- *
- * Your routes tell Sails what to do each time it receives a request.
- *
- * For more information on configuring custom routes, check out:
- * https://sailsjs.com/anatomy/config/routes-js
- */
-
 module.exports.routes = {
   'GET /auth/google': 'AuthController.google',
   'GET /auth/google/callback': 'AuthController.googleCallback',
