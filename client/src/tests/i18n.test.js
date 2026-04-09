26c80df16c1f3425c9a5b8c68e426252133e72b7
RAR <sswwqqaa@gmail.com>
2026-04-09 16:07:12 +0200

    fix: Fixed lang test i8ncluding _virtual dir

--- client/src/tests/i18n.test.js
+++ client/src/tests/i18n.test.js
@@ -149,7 +149,7 @@ describe('i18n translations coverage', () => {
   const languages = fs
     .readdirSync(LOCALES_DIR)
     .filter((d) => fs.statSync(path.join(LOCALES_DIR, d)).isDirectory())
-    .filter((lang) => lang !== BASE_LANG && lang !== 'node_modules');
+    .filter((lang) => lang !== BASE_LANG && lang !== 'node_modules' && lang !== '_virtual');
 
   const baseTranslations = loadLanguage(BASE_LANG);
 
