742d84602eb0c648cd6b5b11e5e8ed2085b5afce
RAR <sswwqqaa@gmail.com>
2026-04-11 01:19:36 +0200

    feat: Changed eslint config locales to have more rules

--- packages/locales/eslint.config.mjs
+++ packages/locales/eslint.config.mjs
@@ -1,12 +1,21 @@
+/* eslint-disable import-x/no-relative-packages, import-x/extensions, no-underscore-dangle */
+import path from 'path';
+
+import baseConfig from '../../eslint-configs/base.mjs';
 import rootConfig from '../../eslint.config.mjs';
 
+const __dirname = import.meta.dirname;
+
 export default [
-  ...rootConfig,
   {
     ignores: ['node_modules', 'dist'],
   },
+  ...baseConfig({
+    packageDir: [__dirname, path.resolve(__dirname, '..')],
+  }),
+  ...rootConfig,
   {
-    files: ['**/*.js'],
+    files: ['src/*/**/*.js'],
     ignores: ['**/index.js'],
     rules: {
       'perfectionist/sort-objects': [
