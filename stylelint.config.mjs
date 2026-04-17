0cfae468a70435ccc08f2b301135ace08676fa02
RAR <sswwqqaa@gmail.com>
2026-04-18 01:31:14 +0200

    fix: Adjusted stylelint config and fixed issues

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -7,17 +7,10 @@ export default {
       files: ['**/*.{css,scss}'],
       customSyntax: 'postcss-scss',
       rules: {
-        'font-family-no-duplicate-names': null,
-        'font-family-no-missing-generic-family-keyword': null,
-        'no-empty-source': null,
-        'property-no-deprecated': null,
-        'scss/at-extend-no-missing-placeholder': null,
-        'selector-class-pattern': null,
-        // 'declaration-no-important': true,
         'selector-pseudo-class-no-unknown': [
           true,
           {
-            ignorePseudoClasses: ['global', 'local'],
+            ignorePseudoClasses: ['global'],
           },
         ],
       },
