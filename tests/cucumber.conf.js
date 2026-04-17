3a2e8017f617f98a1555b915c7286f1a3091f157
RAR <sswwqqaa@gmail.com>
2026-04-18 01:49:14 +0200

    added e2e test for admin login feature Signed-off-by: Madhavi0207 <madhavipaudel86@gmail.com>

---
+++ tests/cucumber.conf.js
@@ -0,0 +1,35 @@
+const { BeforeAll, AfterAll, Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
+const { chromium } = require('playwright');
+
+setDefaultTimeout(60000);
+
+let browser;
+
+BeforeAll(async function () {
+  browser = await chromium.launch({
+    headless: true,
+  });
+});
+
+Before(async function () {
+  this.browser = browser;
+
+  this.context = await this.browser.newContext();
+  this.page = await this.context.newPage();
+});
+
+After(async function () {
+  if (this.page) {
+    await this.page.close();
+  }
+
+  if (this.context) {
+    await this.context.close();
+  }
+});
+
+AfterAll(async function () {
+  if (browser) {
+    await browser.close();
+  }
+});
---
+++ tests/e2e/support/CustomWorld.ts
@@ -0,0 +1,35 @@
+import { IWorldOptions, setWorldConstructor, World } from '@cucumber/cucumber';
+
+import { Browser, BrowserContext, Page } from '@playwright/test';
+
+import { LoginPage } from '../pageObject/LoginPage';
+
+export class CustomWorld extends World {
+  browser!: Browser;
+  context!: BrowserContext;
+  page!: Page;
+
+  loginPage!: LoginPage;
+
+  users: Record<string, unknown> = {};
+  meetings: Record<string, unknown> = {};
+  scenarioData: Record<string, unknown> = {};
+
+  constructor(options: IWorldOptions) {
+    super(options);
+  }
+
+  initPages(): void {
+    this.loginPage = new LoginPage(this.page);
+  }
+
+  setData(key: string, value: unknown): void {
+    this.scenarioData[key] = value;
+  }
+
+  getData<T>(key: string): T {
+    return this.scenarioData[key] as T;
+  }
+}
+
+setWorldConstructor(CustomWorld);
